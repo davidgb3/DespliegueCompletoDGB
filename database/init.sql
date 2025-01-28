c09223ae330578547b7a2cadadfb292a4785c1cc
davidgb3 <gbdavid774@gmail.com>
2025-01-28 17:07:09 +0100

    primer commit David completo

---
+++ database/init.sql
@@ -0,0 +1,6 @@
+CREATE TABLE messages (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    content VARCHAR(255) NOT NULL
+);
+
+INSERT INTO messages (content) VALUES ('La base de datos responde, Holaaa!');
---
+++ frontend/src/App.jsx
@@ -0,0 +1,22 @@
+import { useEffect, useState } from 'react';
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [message, setMessage] = useState('');
+    useEffect(() => {
+    fetch('/api/db')
+      .then((res) => res.json())
+      .then((data) => setMessage(data.message));
+  }, []);
+
+  return (
+    <div>
+      <h1>Frontend en React de David Gómez Bravo</h1>
+      <p>Esta aplicación se conecta al backend de Symfony pidiéndole una respuesta</p>
+      <p>respuesta del Backend: {message || 'Loading...'}</p>
+    </div>
+  );
+}
+export default App;
